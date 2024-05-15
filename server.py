15f133dada323226a551bc5174b2a6a49686e142
Lairlynx <vladut.lazar@student.unitbv.ro>
2024-05-15 11:28:02 +0300

    1.0 Release

--- server.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-HOST = "192.168.0.14"  # Replace with the target PC's IP address
-PORT = 5555
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, this is a notification from the source PC")
-    print("Notification sent successfully")
